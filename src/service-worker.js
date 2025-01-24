8779d73ad7bb9f068549caa7fbdc84973dee2097
Jean lucas <jean__lucas2013@hotmail.com>
2025-01-24 09:00:40 -0300

    teste 1

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,17 +1,21 @@
+/* eslint-env serviceworker */
 
+import { precacheAndRoute } from 'workbox-precaching';
 
-this.addEventListener('install', (event) => {
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+self.addEventListener('install', (event) => {
     console.log('Service Worker instalado');
     // Realize tarefas de instalação aqui, como cache de recursos
   });
   
-  this.addEventListener('activate', (event) => {
+  self.addEventListener('activate', (event) => {
     console.log('Service Worker ativado');
     // Realize tarefas de ativação aqui, como limpar cache antigo
   });
   
   // Este evento é disparado quando uma notificação push é recebida
-  this.addEventListener('push', (event) => {
+  self.addEventListener('push', (event) => {
     const data = event.data ? event.data.json() : {};
     const title = data.notification.title || 'Notificação de Exemplo';
     const options = {
@@ -22,12 +26,12 @@ this.addEventListener('install', (event) => {
     };
   
     event.waitUntil(
-      this.registration.showNotification(title, options)
+      self.registration.showNotification(title, options)
     );
   });
   
   // Este evento é disparado quando o usuário clica na notificação
-  this.addEventListener('notificationclick', (event) => {
+  self.addEventListener('notificationclick', (event) => {
     const url = event.notification.data.url;
     event.notification.close();
   });
